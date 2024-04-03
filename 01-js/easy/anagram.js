c55d8e63c50b2d304edcd72e6a1bd2150a63db0d
KheteshS <kheteshs@gmail.com>
2024-04-04 00:04:22 +0530

    easy assignments completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
+    let str1Arr = str1.toUpperCase().split("").sort();
+    let str2Arr = str2.toUpperCase().split("").sort();
 
+    if (str1Arr.length !== str2Arr.length) {
+        return false;
+    } else {
+        for (let i = 0; i < str1Arr.length; i++) {
+            if (str1Arr[i] !== str2Arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
+// isAnagram("Hello sdfd", "Hello dfds");
 
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -12,9 +12,40 @@
 	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
+const transactions = [
+    {
+        id: 1,
+        timestamp: 1656076800000,
+        price: 10,
+        category: "Food",
+        itemName: "Pizza",
+    },
+    {
+        id: 2,
+        timestamp: 1656163200000,
+        price: 5,
+        category: "Beverages",
+        itemName: "Coffee",
+    },
+    {
+        id: 3,
+        timestamp: 1656249600000,
+        price: 15,
+        category: "Food",
+        itemName: "Burger",
+    },
+];
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+    const result = transactions.reduce((acc, { category, price }) => {
+        if (!acc[category]) {
+            acc[category] = { category, totalSpent: 0 };
+        }
+        acc[category].totalSpent += price;
+        return acc;
+    }, {});
+
+    return Object.values(result);
 }
 
 module.exports = calculateTotalSpentByCategory;
